1b19c422a994938452be8a8109c5f8164711d4ed
mkulas <mkulas@kali>
2020-10-27 14:39:57 +0100

    HOME PAGE SUCCESS

---
+++ Develop/For_Test_Develop.php
@@ -0,0 +1,42 @@
+<?php
+$kategory = [
+    '0' => [
+        'image' => 'Media/Kategory/priroda.jpeg',
+        'Name' => 'PRÍRODA',
+    ],
+
+    '1' => [
+        'image' => 'Media/Kategory/Architektura.jpeg',
+        'Name' => 'ARCHITEKÚRA',
+        'count' => '6 fotiek',
+    ],
+
+    '2' => [
+        'image' => 'Media/Kategory/Ludia.jpg',
+        'Name' => 'ĽUDIA',
+    ],
+
+    '3' => [
+        'image' => 'Media/Kategory/jedlo.jpg',
+        'Name' => 'JEDLO',
+    ],
+
+    '4' => [
+        'image' => 'Media/Kategory/Auta.jpeg',
+        'Name' => 'AUTÁ',
+    ],
+
+
+];
+
+
+/**
+ * <div class="card">
+ * <div class="card-body text-center">
+ * <div class="text-warning mt-4 ml-4 mr-4 mb-1 bold">Teraz pridajte fotky pre túro kategóriu</div>
+ * <img src="" width="40" alt="ikonka add" class="filter-green">
+ * <p class="text-center bold  m-0 card-title">Name</p>
+ * </div>
+ * </div>
+ * </div>
+ **/
--- index.php
+++ index.php
@@ -1,13 +1,57 @@
+
+
+<?php
+/** IBA POMOCKA AŽ NEMUSÍM VYPISOVAŤ VŠETKO V HTML */
+require_once 'Develop/For_Test_Develop.php';
+?>
 <!DOCTYPE html>
 <html lang="sk">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
-    <title>Môj systém</title>
-
+    <title>Uroveň1</title>
+    <link rel="stylesheet" href="Access/Css/bootstrap.css">
+    <link rel="stylesheet" href="Access/Css/style.css">
 </head>
-<body>
+    <body>
+        <!-- HEADER BACKGROUND CONTAINER -->
+        <div class="container-fluid p-0">
+            <div class="headerimage">
+                <img src="Media/Kategory/Architektura.jpeg" alt="Obrázok pozadia" class="rounded w-100 img-fluid">
+            </div>
+        </div>
+        <!-- POSITION KONTAINER AND CARD KATEGORY -->
+        <div class="position-absolute mycontainer">
+            <div class="container">
+                <h2>FOTOGALÉRIA</h2>
+                <h3 class="mt-my">KATEGÓRIE</h3>
+                <hr>
+                <div class="card-deck clearfix">
+                    <div class="mt-2">
+                        <!-- IBA SOM SI POMOHOL AZ NEMUSIM VSETKO OPAKOVANE PISAT DUFAM ZE TO NEVADÍ -->
+                        <?php foreach ($kategory as $kat):?>
+                        <div class="card mt-4">
+                            <img class="card-img-top rounded img-fluid" src="<?= $kat['image'];?>" alt="Kategória obrázkov">
+                            <div class="card-body">
+                                <p class="text-center bold  m-0 card-title"><?= $kat['Name'];?></p>
+                                <p class="card-text mycall text-center"></p>
+                            </div>
+                        </div>
+                        <?php endforeach;?>
+                        <div class="card mt-4 card-block">
+                            <div class="card-body text-center mt-5">
+                                <img src="Media/add.svg" width="40" alt="ikonka add" class="">
+                                <p class="mt-2 text-center addkategory">PRIDAŤ KATEGÓRIU</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- FOOTER -->
+                <p class="myfoot mt-5">webdesign bart.sk</p>
+            </div>
+        </div>
+    </body>
+</html>
